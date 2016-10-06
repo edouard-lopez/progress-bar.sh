13e78f6438ee422dcf67d0ab94a9718f51849824
Édouard Lopez <dev+git@edouard-lopez.com>
2016-10-06 15:56:17 +0200

    create progress-bar

---
+++ README.md
@@ -0,0 +1,10 @@
+# Progress bar  
+
+> Simple progress bar for `bash`, give it a duration and it will do the rest.
+
+### Usage
+
+1. Copy to your code or `source ./progress-bar.sh`
+2. Call
+
+    progress 5
---
+++ progress-bar.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+progress-bar() {
+  local duration=${1}
+
+
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%% %s %s" $(( (($elapsed)*100)/($duration)*100/100 ))  $elapsed $duration; }
+    clean_line() { printf "\r"; }
+
+  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
+      already_done; remaining; percentage
+      sleep 1
+      clean_line
+  done
+}
