0aaca61bd430c4244858ce75534adee5d939d78e
Alois <aloisdegouvello@live.fr>
2016-10-12 23:06:01 +0200

    fix whitespace

--- progress-bar.sh
+++ progress-bar.sh
@@ -3,11 +3,10 @@
 progress-bar() {
   local duration=${1}
 
-
-    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
-    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
-    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
-    clean_line() { printf "\r"; }
+  already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+  remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+  percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+  clean_line() { printf "\r"; }
 
   for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
       already_done; remaining; percentage
