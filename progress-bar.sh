aa0e53d576fac3c507bd54de576729a4e11eb56a
Édouard Lopez <dev+git@edouard-lopez.com>
2016-10-13 10:51:35 +0200

    fix SC2017: Increase precision by replacing a/b*c with a*c/b.

--- progress-bar.sh
+++ progress-bar.sh
@@ -5,7 +5,7 @@ progress-bar() {
 
   already_done() { for ((done=0; done<elapsed; done++)); do printf "▇"; done }
   remaining() { for ((remain=elapsed; remain<duration; remain++)); do printf " "; done }
-  percentage() { printf "| %s%%" $(( ((elapsed)*100)/(duration)*100/100 )); }
+  percentage() { printf "| %s%%" $(( (elapsed*100)/(duration*100)/100 )); }
   clean_line() { printf "\r"; }
 
   for (( elapsed=1; elapsed<=duration; elapsed++ )); do
